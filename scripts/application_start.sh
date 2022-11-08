1c0610087d4188b716e8d13cec33ab63fec28fd4
azeemmd150 <azeem.md03@gmail.com>
2022-11-08 20:47:46 +0530

    new com

---
+++ scripts/application_start.sh
@@ -0,0 +1,6 @@
+!/bin/bash
+
+# Move to the directory
+cd /home/ubuntu/<app-name>
+
+
---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,10 @@
+!/bin/bash
+
+# Move to the directory
+cd /home/ubuntu/<app-name>
+
+# Install dependencies
+npm i
+
+# Build the Code
+npm run build
